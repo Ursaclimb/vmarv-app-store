aa9426bc684686f01a47ea5febc11d0111048bb5
vMarv <vmarv@example.com>
2026-01-16 00:21:35 +0000

    Critical fix: Add exports.sh and use environment variables in docker-compose

---
+++ vmarv-apps-litecoin/exports.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export LITECOIN_RPC_PORT=9332
+export LITECOIN_P2P_PORT=9333
+export LITECOIN_NODE_IP=litecoin
+export LITECOIN_RPC_USER=litecoinrpc
+export LITECOIN_RPC_PASSWORD=changeme
